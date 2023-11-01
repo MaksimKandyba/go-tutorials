9ae79e478e3d28cbfa563e6dca8a51eba94cbe86
kandyba <m.kandyba@comparus.de>
2023-11-01 19:58:56 +0100

    fix emulated error

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -10,7 +10,7 @@ func Hello(name string) (string, error) {
 	if name == "" {
 		return "", errors.New("empty name")
 	}
-	message := fmt.Sprintf(randomFormat())
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
 
